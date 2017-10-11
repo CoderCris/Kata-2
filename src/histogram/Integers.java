fdfeeb9429dffdfe10b11d0a700f5e3566081307
Crisamer Ortega González <crisamer.ortega@gmail.com>
2017-10-11 17:22:16 +0100

    Version 2, código ordenado y mejora del "show", ahora llamado "output".

--- src/histogram/Integers.java
+++ src/histogram/Integers.java
@@ -1,33 +1,52 @@
 package histogram;
 import java.util.HashMap;
+import java.util.Iterator;
 
 import java.util.Map;
 import java.util.Scanner;
 
+/**
+ *
+ * @author Crisa
+ */
 public class Integers {
     
     private int[] data;
     private Map<Integer, Integer> histogram= new HashMap<>();
     
-    public void fill(){
+
+    public void initialize(){
+        input();
+        procedure();
+        output();
+    }
+    
+    private void input() {
         System.out.println("Tamaño del vector:");
         Scanner input = new Scanner(System.in);
         data = new int[input.nextInt()];
         for(int i=0; i<data.length; i++){
             System.out.println("Introduzca elemento número:" + (i+1));
             data[i] = input.nextInt();
+        }
+    }
+    
+    private void procedure() {
+        for (int i = 0; i < data.length; i++) {
             if(histogram.containsKey(data[i])){
                 histogram.put(data[i], histogram.get(data[i])+1);
             }else{
                 histogram.put(data[i], 1);
             }
         }
-        show();
     }
+
     
-    public void show(){
-        for(Integer val : histogram.keySet()){
-            System.out.println(" Valor: " + val +" Repeticiones: " + histogram.get(val));
-        }
-    } 
+    public void output(){
+        histogram.keySet().forEach((val) -> {
+            System.out.println("Valor: " + val + " Repeticiones: " + histogram.get(val));
+        });
+    }
+
+
 }
